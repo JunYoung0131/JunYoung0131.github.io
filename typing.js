45d872401fd8b8d39beca1660a93b63c62c6c912
JunYoung0131 <81363090+JunYoung0131@users.noreply.github.com>
2021-04-09 19:26:22 +0900

    homepage

---
+++ typing.js
@@ -0,0 +1,68 @@
+$(function(){
+  var tag = document.querySelector(".typing1");
+  var app = new Hakademy.util.typing(tag, {
+      text:[
+          "Noh Jun Young"
+      ],
+      color:{
+          apply:false
+      },
+      appear:{
+        startDelay:0,
+        processDelay:0.05,
+        finishDelay:1,
+    },
+    exit:{
+        startDelay:100000,
+        processDelay:0.1,
+        finishDelay:1,
+    },
+      
+  });
+});
+
+$(function(){
+    var tag = document.querySelector(".typing2");
+    var app = new Hakademy.util.typing(tag, {
+        text:[
+            "Hacker and Software Engineer"
+        ],
+        color:{
+            apply:false
+        },
+        appear:{
+          startDelay:1,
+          processDelay:0.05,
+          finishDelay:10,
+      },
+      exit:{
+        startDelay:100000,
+        processDelay:0.1,
+        finishDelay:1,
+    },
+        
+    });
+  });
+
+  $(function(){
+    var tag = document.querySelector(".typing3");
+    var app = new Hakademy.util.typing(tag, {
+        text:[
+            "Kookmin Univ. Information Security Cryptography"
+        ],
+        color:{
+            apply:false
+        },
+        appear:{
+          startDelay:3,
+          processDelay:0.05,
+          finishDelay:5,
+      },
+      exit:{
+        startDelay:100000,
+        processDelay:0.1,
+        finishDelay:1,
+    },
+
+    });
+  });
